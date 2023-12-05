58db974750acd377ffe9e20aee556c9439a589b2
iradbouzidi <b.irad1994@gmail.com>
2023-12-05 19:30:43 +0100

    add env

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_DEV_BACKEND_URL}/api/v1/post`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
